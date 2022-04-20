0775cec272b111b5e1f7b11546f20dc4d2f85a7a
BikkIlshat <heavyilshat@rambler.ru>
2022-04-20 19:38:59 +0500

    1.Реализовал переходы между экранами по нажатию на иконки нижнего меню навигации 2. Для наглядности реализовал временный переход  экрана ленты в экран деталей

--- app/src/main/java/com/agobikk/cookeatenjoy/screens/FeedFragment.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/screens/FeedFragment.kt
@@ -1,9 +1,22 @@
 package com.agobikk.cookeatenjoy.screens
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.agobikk.cookeatenjoy.R
+import com.agobikk.cookeatenjoy.databinding.FragmentFeedBinding
 
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
+    private val viewBinding: FragmentFeedBinding by viewBinding()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewBinding.feedItemView.setOnClickListener {
+            findNavController().navigate(R.id.action_feedFragment_to_detailFragment)
+        }
+    }
 }
--- app/src/main/java/com/agobikk/cookeatenjoy/screens/MainFragment.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/screens/MainFragment.kt
@@ -3,7 +3,10 @@ package com.agobikk.cookeatenjoy.screens
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.agobikk.cookeatenjoy.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
@@ -11,5 +14,11 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val bottomNavigationView =
+            view.findViewById<BottomNavigationView>(R.id.mainBottomNavigationView)
+        val navController =
+            (childFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment)
+                .navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
