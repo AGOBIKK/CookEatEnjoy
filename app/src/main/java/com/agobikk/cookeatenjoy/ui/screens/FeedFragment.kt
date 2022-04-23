4f7176dca89ca7e74c5bd119f4e0fef14ffd3833
BikkIlshat <heavyilshat@rambler.ru>
2022-04-23 23:00:00 +0500

    1. Подготовлен adapter для категории рецептов

--- app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/FeedFragment.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/FeedFragment.kt
@@ -3,17 +3,41 @@ package com.agobikk.cookeatenjoy.ui.screens
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.agobikk.cookeatenjoy.R
-
 import com.agobikk.cookeatenjoy.databinding.FragmentFeedBinding
+import com.agobikk.cookeatenjoy.model.Category
+import com.agobikk.cookeatenjoy.ui.adapters.CategoryAdapter
+import com.agobikk.cookeatenjoy.ui.adapters.OnCategoryClick
+import com.agobikk.cookeatenjoy.util.Const
 
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
     private val viewBinding: FragmentFeedBinding by viewBinding()
+    private lateinit var adapter: CategoryAdapter
+    private lateinit var recyclerView: RecyclerView
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        initRecyclerView()
     }
+
+    private fun initRecyclerView() = with(viewBinding) {
+
+        recyclerView = categoryListRecyclerView
+        adapter = CategoryAdapter(OnCategoryClick {  })
+        recyclerView.adapter = adapter
+
+        val categoryList: List<Category> = List(Const.DEFAULT_CATEGORIES_NAMES.size) { index ->
+            Category(
+                getString(Const.DEFAULT_CATEGORIES_NAMES[index]),
+                Const.DEFAULT_CATEGORY_IMAGES[index]
+            )
+        }
+        adapter.categoryData = categoryList
+    }
+
+
 }
