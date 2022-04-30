ab477622dc67b34814065835b2d65b74b5adc03b
BikkIlshat <heavyilshat@rambler.ru>
2022-05-01 02:49:05 +0500

    1. Проинициализирован IngredientListAdapter, для IngredientListFragment

--- app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/ingredient/IngredientListAdapter.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/ingredient/IngredientListAdapter.kt
@@ -1,38 +1,55 @@
 package com.agobikk.cookeatenjoy.ui.screens.ingredient
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.agobikk.cookeatenjoy.R
+import com.agobikk.cookeatenjoy.databinding.LayoutIngredientsItemBinding
 import com.agobikk.cookeatenjoy.model.ExtendedIngredient
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 
 class IngredientListAdapter(private val onClickListener: IngredientListFragment.OnClickListener) :
-   ListAdapter<ExtendedIngredient, IngredientListAdapter.IngredientViewHolder>(
-        IngredientDiffUtil()
-    ) {
+    ListAdapter<ExtendedIngredient, IngredientListAdapter.IngredientViewHolder>(IngredientDiffUtil()) {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder =
+        IngredientViewHolder(
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.layout_ingredients_item, parent, false)
+        )
 
-
-
-    class IngredientViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-
+    override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.itemView.setOnClickListener { onClickListener.onClick(item) }
+        holder.bind(item)
     }
 
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): IngredientListAdapter.IngredientViewHolder {
-        TODO("Not yet implemented")
+    class IngredientViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val viewBinding: LayoutIngredientsItemBinding by viewBinding()
+        fun bind(extendedIngredient: ExtendedIngredient) = with(viewBinding) {
+            ingredientName.text = extendedIngredient.name
+            ingredientAmount.text = extendedIngredient.amount.toString()
+            ingredientUnit.text = extendedIngredient.unit
+            ingredientConsistency.text = extendedIngredient.consistency
+            ingredientOriginal.text = extendedIngredient.original
+            ingredientImageView.apply {
+                Glide
+                    .with(context)
+                    .setDefaultRequestOptions(
+                        RequestOptions()
+                            .placeholder(R.drawable.loading_animation)
+                            .error(R.drawable.ic_broken_image)
+                    )
+                    .load(extendedIngredient.image)
+                    .into(this)
+            }
+        }
     }
-
-    override fun onBindViewHolder(
-        holder: IngredientListAdapter.IngredientViewHolder,
-        position: Int
-    ) {
-        TODO("Not yet implemented")
-    }
-
-
 }
