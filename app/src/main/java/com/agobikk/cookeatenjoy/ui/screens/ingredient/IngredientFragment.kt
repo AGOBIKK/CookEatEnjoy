402459471a84db152104f7be04f8a11a79f5c154
BikkIlshat <heavyilshat@rambler.ru>
2022-05-01 00:41:35 +0500

    1. Применил шрифты для TextView

--- app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/ingredient/IngredientFragment.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/ingredient/IngredientFragment.kt
@@ -1,9 +1,15 @@
 package com.agobikk.cookeatenjoy.ui.screens.ingredient
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.agobikk.cookeatenjoy.R
 
 
-class IngredientFragment : Fragment() {
+class IngredientFragment : Fragment(R.layout.fragment_ingredient) {
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
