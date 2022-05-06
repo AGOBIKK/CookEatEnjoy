37b4c7472bba68b31d9af45bff3c11341916e6f6
dmitryagoshkov <agodimon@mail.ru>
2022-05-06 12:19:41 +0500

    заполнил fragment_likes.xml по подобию fragment_list_recipe

--- app/src/main/java/com/agobikk/cookeatenjoy/data/remote/api/RemoteInstance.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/data/remote/api/RemoteInstance.kt
@@ -4,14 +4,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RemoteInstance {
-    private val retrofit by lazy { //  создали переменную retrofit будем к ней обращаться
+    private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
-
-    val api: ApiService by lazy {
+    val api:ApiService by lazy {
         retrofit.create(ApiService::class.java)
     }
 }
