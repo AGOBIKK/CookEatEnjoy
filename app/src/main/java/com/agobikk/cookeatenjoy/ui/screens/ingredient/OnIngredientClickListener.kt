9f4e79eb5f614359ba498f78eafe6edd77520246
BikkIlshat <heavyilshat@rambler.ru>
2022-05-10 23:13:53 +0500

    Реализовал слушателя событй (клика) в адаптере  через интерфейс

---
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/ingredient/OnIngredientClickListener.kt
@@ -0,0 +1,7 @@
+package com.agobikk.cookeatenjoy.ui.screens.ingredient
+
+import com.agobikk.cookeatenjoy.model.ExtendedIngredient
+
+interface OnIngredientClickListener {
+    fun onClick(extendedIngredient: ExtendedIngredient)
+}
