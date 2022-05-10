67103e3fc2772fdb4a8b14517faa7dadac4c11b0
BikkIlshat <heavyilshat@rambler.ru>
2022-05-10 20:41:09 +0500

    Убрал в отдельный метод обработку HTML  текста для читаемости метода setDetails в DetailRecipeFragment.kt

--- app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/ingredient/IngredientViewModel.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/ingredient/IngredientViewModel.kt
@@ -1,6 +1,15 @@
 package com.agobikk.cookeatenjoy.ui.screens.ingredient
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.agobikk.cookeatenjoy.data.remote.RemoteRepository
+import com.agobikk.cookeatenjoy.model.ExtendedIngredient
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class IngredientViewModel : ViewModel() {
+    private val repository = RemoteRepository()
+
+
 }
