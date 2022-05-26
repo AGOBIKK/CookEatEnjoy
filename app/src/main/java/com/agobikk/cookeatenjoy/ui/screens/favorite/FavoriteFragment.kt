aeb3527fd7ed4603fe2db9a6d9660ebaad25fb17
BikkIlshat <heavyilshat@rambler.ru>
2022-05-26 10:52:09 +0500

    Подготовка к реализации сохранения  экрана favorite recipe

--- app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/favorite/FavoriteFragment.kt
@@ -4,32 +4,37 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.agobikk.cookeatenjoy.R
 import com.agobikk.cookeatenjoy.databinding.FragmentListRecipeBinding
 import com.agobikk.cookeatenjoy.models.ResultMainCourse
+import com.agobikk.cookeatenjoy.ui.screens.recipe.OnClickListener
+import com.agobikk.cookeatenjoy.ui.screens.recipe.RecipesAdapter
 
 
 class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
     private val viewBinding: FragmentListRecipeBinding by viewBinding()
-    private var adapter: FavoriteAdapter? = null
+    private var adapter: RecipesAdapter? = null
+
+
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (requireActivity() as AppCompatActivity).supportActionBar?.show()
-        init()
+//        init()
     }
 
-    private fun init() {
-        adapter = FavoriteAdapter(object : OnFavoriteClickListener {
-            override fun onClick(resultMainCourse: ResultMainCourse) {
-                navigate()
-            }
-        })
-        viewBinding.recipeListRecyclerView.adapter = adapter
-    }
+//    private fun init() {
+//        adapter = RecipesAdapter(object : OnClickListener {
+//            override fun onClick(resultMainCourse: ResultMainCourse) {
+//                navigate()
+//            }
+//        })
+//        viewBinding.recipeListRecyclerView.adapter = adapter
+//    }
 
 
     private fun navigate() {
--- app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/favorite/FavoriteViewModel.kt
@@ -2,7 +2,11 @@ package com.agobikk.cookeatenjoy.ui.screens.favorite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.agobikk.cookeatenjoy.models.ModelMainCourse
+import retrofit2.Response
 
-class FavoriteViewModel:ViewModel() {
+class FavoriteViewModel():ViewModel() {
+
+
 
 }
