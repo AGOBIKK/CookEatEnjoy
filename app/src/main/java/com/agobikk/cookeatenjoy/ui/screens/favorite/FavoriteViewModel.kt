8bcf673db0cc79cb0975598bb0e64b3e6f043c58
BikkIlshat <81274572+BikkIlshat@users.noreply.github.com>
2022-05-26 17:27:46 +0500

    Revert "Save favorite recipe v1"

--- app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/favorite/FavoriteViewModel.kt
@@ -1,25 +1,12 @@
 package com.agobikk.cookeatenjoy.ui.screens.favorite
 
-import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.agobikk.cookeatenjoy.aplication.di.AssistedSavedStateViewModelFactory
-import com.agobikk.cookeatenjoy.data.remote.RemoteRepository
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedFactory
-import dagger.assisted.AssistedInject
+import com.agobikk.cookeatenjoy.models.ModelMainCourse
+import retrofit2.Response
+
+class FavoriteViewModel():ViewModel() {
 
 
-class FavoriteViewModel @AssistedInject constructor(
-    private val repository: RemoteRepository,
-    @Assisted private val savedStateHandle: SavedStateHandle
-) : ViewModel() {
 
-    @AssistedFactory
-    interface Factory : AssistedSavedStateViewModelFactory<FavoriteViewModel> {
-        override fun create(savedStateHandle: SavedStateHandle): FavoriteViewModel
-    }
-
-    fun getFavoriteRecipe() {
-
-    }
 }
