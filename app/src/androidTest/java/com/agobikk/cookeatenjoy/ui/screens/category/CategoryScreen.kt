1f47eae4ac7ff8f384a06d7eb5c42fc8fc49baad
dmitryagoshkov <agodimon@mail.ru>
2022-06-04 15:41:43 +0500

    написал сценарий теста проверки перехода по экранам

--- app/src/androidTest/java/com/agobikk/cookeatenjoy/ui/screens/category/CategoryScreen.kt
+++ app/src/androidTest/java/com/agobikk/cookeatenjoy/ui/screens/category/CategoryScreen.kt
@@ -2,14 +2,18 @@ package com.agobikk.cookeatenjoy.ui.screens.category
 
 import android.view.View
 import com.agobikk.cookeatenjoy.R
+import com.agobikk.cookeatenjoy.ui.screens.detail.DetailRecipeFragment
+import com.agobikk.cookeatenjoy.ui.screens.ingredient.IngredientListFragment
+import com.agobikk.cookeatenjoy.ui.screens.recipe.RecipeListFragment
 import com.kaspersky.kaspresso.screens.KScreen
 import io.github.kakaocup.kakao.image.KImageView
 import io.github.kakaocup.kakao.recycler.KRecyclerItem
 import io.github.kakaocup.kakao.recycler.KRecyclerView
+import io.github.kakaocup.kakao.text.KButton
 import io.github.kakaocup.kakao.text.KTextView
 import org.hamcrest.Matcher
 
-object CategoryScreen:KScreen<CategoryScreen>() {
+object CategoryScreen : KScreen<CategoryScreen>() {
     override val layoutId: Int? = R.layout.fragment_category
     override val viewClass: Class<*>? = CategoryFragment::class.java
 
@@ -17,9 +21,10 @@ object CategoryScreen:KScreen<CategoryScreen>() {
         itemType(::DefaultItem)
     })
 
-    val predefinedItemsRecycler = KRecyclerView({ withId(R.id.categoryListRecyclerView) }, itemTypeBuilder = {
-        itemType(::CategoryItem)
-    })
+    val predefinedItemsRecycler =
+        KRecyclerView({ withId(R.id.categoryListRecyclerView) }, itemTypeBuilder = {
+            itemType(::CategoryItem)
+        })
 
     class DefaultItem(parent: Matcher<View>) : KRecyclerItem<DefaultItem>(parent) {
         val icon = KImageView { withId(R.id.category_image) }
@@ -31,3 +36,65 @@ object CategoryScreen:KScreen<CategoryScreen>() {
     }
 
 }
+
+object RecipeListScreen : KScreen<RecipeListScreen>() {
+    override val layoutId: Int? = R.layout.fragment_list_recipe
+    override val viewClass: Class<*>? = RecipeListFragment::class.java
+
+    val recyclerRecipe =
+        KRecyclerView({ withId(R.id.recipe_list_recyclerView) }, itemTypeBuilder = {
+            itemType(::DefaultItem)
+        })
+
+    val predefinedItemsRecycler =
+        KRecyclerView({ withId(R.id.recipe_list_recyclerView) }, itemTypeBuilder = {
+            itemType(::RecipeItem)
+        })
+
+    class DefaultItem(parent: Matcher<View>) : KRecyclerItem<DefaultItem>(parent) {
+        val icon = KImageView { withId(R.id.recipe_list_image) }
+        val recipeListTitleTextView = KTextView { withId(R.id.recipe_list_title_text_view) }
+    }
+
+    class RecipeItem(parent: Matcher<View>) : KRecyclerItem<DefaultItem>(parent) {
+        val item = KTextView { withText("Main course") }
+    }
+
+}
+object DetailScreen : KScreen<DetailScreen>() {
+    override val layoutId: Int? = R.layout.fragment_detail_recipe
+    override val viewClass: Class<*>? = DetailRecipeFragment::class.java
+
+    val ingredientButton = KButton { withId(R.id.ingredient_image) }
+    val image = KImageView{withId(R.id.recipe_detail_image)}
+
+}
+
+object IngredientScreen : KScreen<IngredientScreen>() {
+    override val layoutId: Int? = R.layout.fragment_list_ingredient
+    override val viewClass: Class<*>? = IngredientListFragment::class.java
+
+    val recyclerIngredientList =
+        KRecyclerView({ withId(R.id.ingredients_recyclerview) }, itemTypeBuilder = {
+            itemType(::DefaultItem)
+        })
+
+    val predefinedItemsRecycler =
+        KRecyclerView({ withId(R.id.ingredients_recyclerview) }, itemTypeBuilder = {
+            itemType(::RecipeItem)
+        })
+
+    class DefaultItem(parent: Matcher<View>) : KRecyclerItem<DefaultItem>(parent) {
+        val icon = KImageView { withId(R.id.ingredient_image) }
+        val ingredientTitleTextView = KTextView { withId(R.id.ingredient_name) }
+    }
+
+    class RecipeItem(parent: Matcher<View>) : KRecyclerItem<DefaultItem>(parent) {
+        val item = KTextView { withText("Main course") }
+    }
+
+}
+
+
+
+
--- app/src/androidTest/java/com/agobikk/cookeatenjoy/ui/screens/category/CategoryTest.kt
+++ app/src/androidTest/java/com/agobikk/cookeatenjoy/ui/screens/category/CategoryTest.kt
@@ -1,22 +1,99 @@
 package com.agobikk.cookeatenjoy.ui.screens.category
 
 import android.app.Activity
+import androidx.compose.ui.test.hasText
 import androidx.fragment.app.testing.FragmentScenario
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.rule.ActivityTestRule
 import com.agobikk.cookeatenjoy.MainActivity
 import com.agobikk.cookeatenjoy.R
+import com.agobikk.cookeatenjoy.ui.screens.ingredient.IngredientListAdapter
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import kotlinx.coroutines.delay
+import okhttp3.internal.wait
+import org.junit.Rule
 import org.junit.Test
+import java.util.*
 
-class CategoryTest: TestCase(kaspressoBuilder = Kaspresso.Builder.simple().apply {
+class CategoryTest : TestCase(kaspressoBuilder = Kaspresso.Builder.simple().apply {
     beforeEachTest {
         ActivityScenario.launch(MainActivity::class.java)
     }
-})  {
+}) {
+
+    @get:Rule
+    val activityTestRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @org.junit.Test
+    fun activity_AssertNotNull() {
+        ActivityScenario.ActivityAction<MainActivity> {
+            junit.framework.TestCase.assertNotNull(it)
+        }
+    }
+
+    @Test
+    fun activity_IsResumed() {
+        ActivityScenario.ActivityAction<MainActivity> {
+            junit.framework.TestCase.assertEquals(
+                Lifecycle.State.RESUMED, activityTestRule.scenario.state
+            )
+        }
+    }
+
+
+    @Test
+    fun test() =
+        run {
+            step("Open Simple Screen") {
+                activityTestRule.scenario
+                testLogger.i("I am testLogger")
+                CategoryScreen {
+                    recycler.act {
+                        /** Ниже код экшен в стиле Espresso. Если не получается сделать что-то средствами Kakao,
+                         * можно использовать Espresso над Kakao-обёртками с помощью act для экшенов или assert для ассершенов
+                         */
+                        actionOnItem<CategoryAdapter.CategoryViewHolder>(
+                            ViewMatchers.hasDescendant(
+                                ViewMatchers.withText(
+                                    R.string.mainCourse
+                                )
+                            ), ViewActions.click()
+                        )
+                    }
+                }
+                RecipeListScreen {
+                    recyclerRecipe.act {
+                        actionOnItem<CategoryAdapter.CategoryViewHolder>(
+                            ViewMatchers.hasDescendant(
+                                ViewMatchers.withText(
+                                    "Homemade Garlic and Basil French Fries"
+                                )
+                            ), ViewActions.click()
+                        )
+                    }
+                }
+                DetailScreen {
+                    ingredientButton { click() }
+
+                }
+                IngredientScreen {
+                    recyclerIngredientList.scrollToEnd()
+                }
+
+
+            }
+        }
+
 
     @Test
     fun espressoStyleClick() = run {
@@ -27,7 +104,7 @@ class CategoryTest: TestCase(kaspressoBuilder = Kaspresso.Builder.simple().apply
                  */
                 actionOnItem<CategoryAdapter.CategoryViewHolder>(
                     ViewMatchers.hasDescendant(
-                        ViewMatchers.withText (
+                        ViewMatchers.withText(
                             R.string.mainCourse
                         )
                     ), ViewActions.click()
