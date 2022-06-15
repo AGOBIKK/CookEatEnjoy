4fec290cf3887741f76c9ee34deb152f31133817
BikkIlshat <heavyilshat@rambler.ru>
2022-06-15 13:15:57 +0500

    добавил toolbar для SearchFragment.kt

--- app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/SearchFragment.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/SearchFragment.kt
@@ -1,9 +1,24 @@
 package com.agobikk.cookeatenjoy.ui.screens
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.agobikk.cookeatenjoy.R
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        displayHomeUp(true)
+    }
+
+    private fun displayHomeUp(show: Boolean) {
+        requireActivity().run {
+            (this as AppCompatActivity).supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+            supportActionBar?.setCustomView(R.layout.custom_toolbar_search_fragment)
+        }
+    }
 }
