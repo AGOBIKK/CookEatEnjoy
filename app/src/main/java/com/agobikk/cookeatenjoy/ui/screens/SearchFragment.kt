99acdece990f20ab581d173fb64e9acac8825569
BikkIlshat <heavyilshat@rambler.ru>
2022-06-15 13:37:26 +0500

    Добился корректного отображения toolbar во фрагментах при переходах

--- app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/SearchFragment.kt
+++ app/src/main/java/com/agobikk/cookeatenjoy/ui/screens/SearchFragment.kt
@@ -12,6 +12,7 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
         displayHomeUp(true)
     }
 
